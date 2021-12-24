56a5663a98d29b6e4bdb41ab06a8f4fbc253f694
soso2234 <mingi371@naver.com>
2021-12-24 11:33:05 +0900

    최신 업데이트

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-const container = document.querySelector(".image-container")
+const container   = document.querySelector(".image-container")
 const startButton = document.querySelector(".start-button")
-const gameText = document.querySelector(".game-text")
-const playTime = document.querySelector(".play-time")
+const gameText    = document.querySelector(".game-text")
+const playTime    = document.querySelector(".play-time")
 
-const tilesCount = 16;
+const tileCount = 16;
+
 let tiles = [];
+const dragged = {
+    el: null,
+    class : null,
+    index : null,
+}
+let isPlaying = false;
 
-tiles = createImageTiles();
 
-shuffle(tiles).forEach(tiles => container.appendChild(tiles))
+// function
+function checkStatus(){
+    const currentList = [...container.children];
+    const unMatchedList = currentList.filter((child, index) =>  Number(child.getAttribute("data-index")) !== index)
+    if(unMatchedList.length === 0){
+        gameText.getElementsByClassName.display = "block";
+        isPlaying = false;
+    }
+}
 
+function setGame(){
+    isPlaying = true;
+    container.innerHTML = "";
+    tiles = createImageTiles();
+    tiles.forEach(tiles => container.appendChild(tiles))
+    setTimeout(()=>{
+        container.innerHTML = "";
+        shuffle(tiles).forEach(tiles => container.appendChild(tiles))
+    }, 2000)
+}
 
 function createImageTiles(){
     const tempArray = [];
-    Array(tilesCount).fill().forEach((_, i) => {
-        const li = document.createElement("li")
+    Array(tileCount).fill().forEach( (_, i) => {
+        const li = document.createElement("li");
         li.setAttribute('data-index', i)
-        li.classList.add('list${i}');
+        li.setAttribute('draggable', 'true');
+        li.classList.add('list' + i);
         console.log(li)
         tempArray.push(li)
     })
@@ -33,5 +58,41 @@ function shuffle(array){
     return array;
 }
 
-//27분01초
+//events
+container.addEventListener('dragstart', e => {
+    if(isPlaying) return;
+    const obj = e.target
+    dragged.el = obj;
+    dragged.class = obj.className;
+   dragged.index = [ ...obj.parentNode.children].indexOf(obj);
+})
+container.addEventListener('dragover', e => {
+    e.preventDefault()
+})
+container.addEventListener('drop', e => {
+    if(!isPlaying) return;
+    const obj = e.target;
+
+    if(obj.className !== dragged.class) {
+        let originPlace;
+        let isLast = false;
+
+        if(dragged.el.nextSibling){
+            originPlace = dragged.el.nextSibling
+        } else {
+            originPlace = dragged.el.previousSibling
+            isLast = true;
+        }
+        const droppedIndex = [ ...obj.parentNode.children].indexOf(obj);
+        dragged.index > droppedIndex ? obj.before(dragged.el) : obj.after(dragged.el)
+        isLast ? originPlace.after(obj) : originPlace.before(obj)
+    }
+    checkStatus();
+})
+
+startButton.addEventListener('click', () => {
+    setGame()
+})
+
+//54분
 //https://www.youtube.com/watch?v=iTBZdg7tg-w
